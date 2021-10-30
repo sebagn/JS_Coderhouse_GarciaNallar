10c883eb9954129462006cfa885750ae6733024d
Sebastian Garcia Nallar <sebagn@hotmail.com>
2021-10-30 09:39:08 -0300

    3er entrega, ajax no funcionando

---
+++ planes.js
@@ -0,0 +1,15 @@
+$("#botonMostrar").click(() => {
+    $.get("planes.json", function(respuesta,estado){
+        if (estado === "success"){
+            let data = respuesta;
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i];
+                $("#nuestrosPlanes").append(
+                    `<div class="hidden tarjeta"><h3>Plan ${element.tipo} ${element.precio} </h3></div>`
+                )
+                $(".hidden").slideDown("slow");
+            }
+        }   
+    })
+})
